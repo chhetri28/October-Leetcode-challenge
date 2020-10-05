83a16ccb76bd614c91a7307c91c4dfed145c0d75
chhetri28 <chettrisourav28@gmail.com>
2020-10-05 14:35:59 +0530

    Day4

---
+++ Day4.cpp
@@ -0,0 +1,65 @@
+//   Remove Covered Intervals
+/*
+Given a list of intervals, remove all intervals that are covered by another interval in the list.
+
+Interval [a,b) is covered by interval [c,d) if and only if c <= a and b <= d.
+
+After doing so, return the number of remaining intervals.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,4],[3,6],[2,8]]
+Output: 2
+Explanation: Interval [3,6] is covered by [2,8], therefore it is removed.
+
+Example 2:
+
+Input: intervals = [[1,4],[2,3]]
+Output: 1
+
+Example 3:
+
+Input: intervals = [[0,10],[5,12]]
+Output: 2
+
+Example 4:
+
+Input: intervals = [[3,10],[4,10],[5,11]]
+Output: 2
+
+Example 5:
+
+Input: intervals = [[1,2],[1,4],[3,4]]
+Output: 1
+
+ 
+
+Constraints:
+
+    1 <= intervals.length <= 1000
+    intervals[i].length == 2
+    0 <= intervals[i][0] < intervals[i][1] <= 10^5
+    All the intervals are unique.
+
+*/
+class Solution {
+public:
+	int removeCoveredIntervals(vector<vector<int>>& intervals) {
+		int res=intervals.size();
+		auto comp=[](auto &v1, auto &v2){
+			return v1[0]<v2[0];
+		};
+		sort(intervals.rbegin(),intervals.rend(),comp);
+		for(int i=0;i<intervals.size()-1;i++){
+			for(int j=i+1;j<intervals.size();j++){   
+				if(intervals[i][0]>=intervals[j][0] && intervals[i][1]<=intervals[j][1]){
+					res--;
+					break;
+				}
+			}
+		}
+		return res;
+	}
+};
